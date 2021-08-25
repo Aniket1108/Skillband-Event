9201c97a029f4dccc4e5c769c4a10a063fc64903
Aniket Gholap <80505096+Aniket1108@users.noreply.github.com>
2021-08-25 14:55:30 +0530

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 const config={
-    uri:'mongodb+srv://skillband303:skillband123@cluster0.s7i2h.mongodb.net/TheUltimatePerformer?retryWrites=true&w=majority'
+    uri:'mongouri'
 }
 module.exports=config;
 
